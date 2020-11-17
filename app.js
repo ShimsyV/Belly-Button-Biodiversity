1c4fcf1d5702155d679db9517c9a219b29d491f9
Shimsy Varkey <shimsyanna@gmail.com>
2020-11-16 22:27:32 -0600

    Edited code to create the function for the initial data rendering

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-// getting data from the json file
-// creating function for data plotting 
-function getPlot(id) {
+// create the function for the initial data rendering
+function init() {
+    // select dropdown menu 
+    var dropdown = d3.select("#selDataset");
+
+    // read the data 
     d3.json("data/samples.json").then((data) => {
         console.log(data)
 
-    })
-
+    });
 }
+
+init();
